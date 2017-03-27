28a7272f920b01b2a18fa6223c97d25796dacc4c
Sam Wighton <sam@samuelwighton.com>
2017-03-27 12:17:23 +1100

    Updated node server and rust version number

--- src/node_server.js
+++ src/node_server.js
@@ -1,12 +1,12 @@
 const dgram = require('dgram');
-const message = Buffer.from('hiive_hiive collection 345');
+const message = Buffer.from('get_all_edges hiive_hiive staff 1000 manages');
 const client = dgram.createSocket('udp4');
 client.send(message, 34254, 'localhost', (err) => {
-	  client.close();
+	client.close();
 });
 
 
-var WebSocketServer = require('websocket').server;
-wsServer = new WebSocketServer({
-	httpServer: server
-});
+// var WebSocketServer = require('websocket').server;
+// wsServer = new WebSocketServer({
+	// httpServer: server
+// });
